c95c720d5334ca030c8504f5289c9f5227e7705b
Younis-Matouq <matoquyounes@gmail.com>
2023-12-03 21:43:34 -0500

    Finalized the different color extractor script

--- extracting_calibration_boards_diff_color.py
+++ extracting_calibration_boards_diff_color.py
@@ -4,16 +4,20 @@ import os
 import numpy as np
 import cv2
 from pathlib import Path
+from src.utiles import *
 
-def extracting_calibration_boards_same_color(imgs_directory_path,save_path):
+def extracting_calibration_boards_diff_color(imgs_directory_path,save_path):
     '''
-    This function was designed for processing a set of images to identify and extract regions corresponding to specific color ranges.
-    It operates on a directory of .png images, applying color segmentation based on predefined color ranges.
-    For each image, the function applies Gaussian blurring, converts the image to the HSV color space, and then creates masks for each color range.
-    It calculates the area of the masked regions, filters out regions below a certain size threshold, and finds contours within these regions.
-    Finally, it generates filled polygon masks for these contours and saves these masks as separate files.
-        
+    This function was designed for processing a set of images to identify and extract regions corresponding to specific color 
+    ranges. It operates on a directory of .png images, applying color segmentation based on predefined color ranges.
+    For each image, the function applies Gaussian blurring, converts the image to the HSV color space,
+    and then creates masks for each color range. It calculates the area of the masked regions, filters
+    out regions below a certain size threshold, and finds contours within these regions. Finally, it generates filled polygon
+    masks for these contours and saves these masks as separate files.        
     '''
+    #initiat color ranges
+    colors= color_range_initiator()
+    #get images paths
     all_imgs_path=glob.glob(os.path.join(imgs_directory_path,'*.png'))
 
     for img_path in all_imgs_path:
@@ -41,3 +45,13 @@ def extracting_calibration_boards_same_color(imgs_directory_path,save_path):
 
             
             save_filled_mask(filled_polygon_mask, file_path= os.path.join(save_path,Path(img_path).stem+f'_mask_{i}'+Path(img_path).suffix))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process images to extract calibration boards that has different colors.")
+    parser.add_argument("imgs_directory_path", help="Path to the directory containing images.")
+    parser.add_argument("save_path", help="Path where the output masks will be saved.")
+
+    args = parser.parse_args()
+    
+    extracting_calibration_boards_diff_color(args.imgs_directory_path, args.save_path)
